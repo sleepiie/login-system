0762910bdeacb6f73ba4f2384860e0079d89a8f8
Peerapart Namnuch <Peerapart.namnuch@hotmail.com>
2026-04-20 16:30:00 +0700

    add auth service

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.25.0
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.50.0 // indirect
 )
--- usecase/auth.go
+++ usecase/auth.go
@@ -1,1 +1,51 @@
 package usecase
+
+import (
+	"time"
+
+	"github.com/sleepiie/login-system/domain"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthUseCase interface {
+	Login(username, password string) (string, error)
+}
+
+type authService struct {
+	userRepo  domain.UserRepository
+	secretKey string
+}
+
+func NewAuthService(repo domain.UserRepository, secret string) AuthUseCase {
+	return &authService{
+		userRepo:  repo,
+		secretKey: secret,
+	}
+}
+
+func (s *authService) Login(username, password string) (string, error) {
+	user, err := s.userRepo.FindByUsername(username)
+	if err != nil {
+		return "", domain.ErrInvalidCredentials
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return "", domain.ErrInvalidCredentials
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub":      user.ID,
+		"username": user.Username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(s.secretKey))
+	if err != nil {
+		return "", domain.ErrInternalServer
+	}
+
+	return tokenString, nil
+}
