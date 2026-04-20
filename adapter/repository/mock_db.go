7e2c95dd8ded5dac8dd756faadd5039566eb2bca
Peerapart Namnuch <Peerapart.namnuch@hotmail.com>
2026-04-20 16:44:34 +0700

    add mockdb

--- adapter/repository/mock_db.go
+++ adapter/repository/mock_db.go
@@ -1,1 +1,33 @@
 package repository
+
+import (
+	"github.com/sleepiie/login-system/domain"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type mockUserRepository struct {
+	users map[string]*domain.User
+}
+
+func NewMockUserRepository() domain.UserRepository {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("password123"), bcrypt.DefaultCost)
+
+	return &mockUserRepository{
+		users: map[string]*domain.User{
+			"admin": {
+				ID:       "1",
+				Username: "admin",
+				Password: string(hashedPassword),
+			},
+		},
+	}
+}
+
+func (m *mockUserRepository) FindByUsername(username string) (*domain.User, error) {
+	user, exists := m.users[username]
+	if !exists {
+		return nil, domain.ErrUserNotFound
+	}
+	return user, nil
+}
