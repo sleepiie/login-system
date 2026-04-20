79e767a54610f36b3b45ce34541696a77785ba88
Peerapart Namnuch <Peerapart.namnuch@hotmail.com>
2026-04-20 17:08:37 +0700

    add http handler and complete main.go

--- adapter/handler/http_handler.go
+++ adapter/handler/http_handler.go
@@ -1,1 +1,47 @@
 package handler
+
+import (
+	"github.com/sleepiie/login-system/usecase"
+
+	"encoding/json"
+	"net/http"
+)
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type AuthHandler struct {
+	authUseCase usecase.AuthUseCase
+}
+
+func NewAuthHandler(uc usecase.AuthUseCase) *AuthHandler {
+	return &AuthHandler{authUseCase: uc}
+}
+
+func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	// เรียกใช้งาน Core Logic
+	token, err := h.authUseCase.Login(req.Username, req.Password)
+	if err != nil {
+		http.Error(w, "Unauthorized: "+err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"token": token,
+	})
+}
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sleepiie/login-system/adapter/handler"
+	"github.com/sleepiie/login-system/adapter/repository"
+	"github.com/sleepiie/login-system/usecase"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: No .env file found, using system environment variables")
+	}
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("FATAL ERROR: JWT_SECRET is not set in .env")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	userRepo := repository.NewMockUserRepository()
+
+	authUseCase := usecase.NewAuthService(userRepo, jwtSecret)
+
+	authHandler := handler.NewAuthHandler(authUseCase)
+
+	http.HandleFunc("/login", authHandler.Login)
+
+	fmt.Printf("Server is running on http://localhost:%s\n", port)
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
